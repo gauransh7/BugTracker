9d79a0177854bcf24fa0cebf90dbb135be964306
gauransh7 <gauransh7dingwani@gmail.com>
2020-05-03 04:33:49 +0530

    3 models created

--- imgsummer2020/BugTracker/admin.py
+++ imgsummer2020/BugTracker/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Project,Bug
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Bug)
+
--- imgsummer2020/BugTracker/models.py
+++ imgsummer2020/BugTracker/models.py
@@ -16,3 +16,44 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ManyToManyField(User)
+    wiki = models.TextField()
+    status = models.BooleanField(default = 1)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
+
+
+STATUS_CHOICES = (
+    ('new','New'),
+    ('assigned','Assigned'),
+    ('duplicate','Duplicate'),
+    ('not a bug','Not a Bug'),
+    ('open','Open'),
+    ('fixed','Fixed'),
+    ('retesting','Re-testing'),
+    ('verified','Verified'),
+    ('closed','Closed'),
+)
+
+class Bug(models.Model):
+    heading = models.CharField(max_length=500)
+    user = models.ForeignKey(User, null=True ,on_delete = models.SET_NULL)
+    listed_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now = True)
+    status = models.CharField(max_length=15,choices = STATUS_CHOICES, default = 'New')
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    description = models.CharField(max_length = 10000)
+    
+    class Meta:
+        ordering = ['-listed_on']
+
+    def __str__(self):
+        return self.heading
