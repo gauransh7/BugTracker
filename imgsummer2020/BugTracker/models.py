b4811931b77791d01229cd886440a82e1dabc6f6
gauransh7 <gauransh7dingwani@gmail.com>
2020-05-03 00:57:53 +0530

    user model created

---
+++ imgsummer2020/BugTracker/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_images')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
